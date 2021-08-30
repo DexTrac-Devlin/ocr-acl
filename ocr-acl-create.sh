f45ab4a6fdd0d9402cff18b9ccda840556e98cd1
devlintrace <mintmining@gmail.com>
2021-08-30 15:28:38 -0500

    cleanup

--- ocr-acl-create.sh
+++ ocr-acl-create.sh
@@ -109,6 +109,7 @@ sed -i -e 's|/ip4/||g' peer_ip_addrs
 sed -i 's|/dns4/.*||' peer_ip_addrs
 sed -i 's|/tcp/|, |' peer_ip_addrs
 sed -i '/^$/d' peer_ip_addrs
+sed -z -i 's/\n/,/g;s/,$/\n/' peer_ip_addrs
 }
 
 # =========
@@ -123,32 +124,13 @@ iptables -A INPUT -p tcp --dport $LISTENPORT -j REJECT
 # =========
 # Create Bash Script for Automation Purposes
 make_shell_script () {
-cat <<EOT >> ocr-acl.sh
-# Collect Database Variables
-source $WORKINGDIR/envVars
-
-# Collect Known Peers
-PGPASSWORD="$PGPASSWORD" psql -U $PGUSERNAME -d $PGDATABASE -h $PGHOSTORIP -p 5432 -c '\copy (SELECT addr FROM p2p_peers) TO 'peer_ip_addrs' CSV;'
-echo "Formatting Peer Information."
-sed -i -e 's|/ip4/||g' peer_ip_addrs
-sed -i 's|/dns4/.*||' peer_ip_addrs
-sed -i 's|/tcp/|, |' peer_ip_addrs
-sed -i '/^$/d' peer_ip_addrs
-
-# Create whitelist for known ocr peers
-while read ip port; do
-    iptables -A INPUT -s $ip -p tcp --dport $LISTENPORT -j ACCEPT
-done < "peer_ip_addrs"
-iptables -A INPUT -p tcp --dport $LISTENPORT -j REJECT
-# Dedupe rules
-iptables-save | sort -u | uniq | iptables-restore
-EOT
+sudo chmod +x $WORKING_DIR/ocr-acl.sh
 }
 
 # =========
 # create cronjob to run the above script
 add_to_crontab () {
-(crontab -l 2>/dev/null; echo "0 * * * * $WORKINGDIR/ocrwhitelistcron.sh") | crontab -
+(crontab -l 2>/dev/null; echo "0 * * * * $WORKING_DIR/ocr-acl.sh") | crontab -
 }
 
 # ==================
--- ocr-acl.sh
+++ ocr-acl.sh
@@ -1,17 +1,21 @@
 #!/bin/bash
+WORKING_DIR=$(pwd)
+
 # Collect Database Variables
-source $WORKINGDIR/envVars
+source $WORKING_DIR/envVars
+
 # Collect Known Peers
-PGPASSWORD="$PGPASSWORD" psql -U $PGUSERNAME -d $PGDATABASE -h $PGHOSTORIP -p 5432 -c '\copy (SELECT addr FROM p2p_peers) TO 'peer_ip_addrs' CSV;'
+PGPASSWORD=$PGPASSWORD psql -U $PGUSERNAME -d $PGDATABASE -h $PGHOSTORIP -p 5432 -c '\copy (SELECT addr FROM p2p_peers) TO 'peer_ip_addrs' CSV;'
 echo "Formatting Peer Information."
 sed -i -e 's|/ip4/||g' peer_ip_addrs
 sed -i 's|/dns4/.*||' peer_ip_addrs
-sed -i 's|/tcp/|, |' peer_ip_addrs
+sed -i 's|/tcp/.*||' peer_ip_addrs
 sed -i '/^$/d' peer_ip_addrs
+sed -z -i 's/\n/,/g;s/,$/\n/' peer_ip_addrs
+
 # Create whitelist for known ocr peers
-while read ip port; do
-    iptables -A INPUT -s $ip -p tcp --dport $LISTENPORT -j ACCEPT
-done < "peer_ip_addrs"
-iptables -A INPUT -p tcp --dport $LISTENPORT -j REJECT
+peerips='cat peer_ip_addrs'
+sudo iptables -A INPUT -s $peerips -p tcp --dport $LISTENPORT -j ACCEPT
+sudo iptables -A INPUT -p tcp --dport $LISTENPORT -j REJECT
 # Dedupe rules
-iptables-save | sort -u | uniq | iptables-restore
+sudo iptables-save | sort -u | uniq | sudo iptables-restore
